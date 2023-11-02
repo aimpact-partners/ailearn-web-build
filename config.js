5cd4cb9426f32f57dfc57bb69385fd287dd2418a
ftovar8 <felix.godo@gmail.com>
2023-11-02 09:49:40 -0300

    feat: publish v0.0.12 11/02/23 02

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ System.register([], function (_export, _context) {
             "key": "ebe05ca0a4d9aafb4024d0666471a0c7",
             "id": "pNInz6obpgDQGcFmaJgB"
           },
-          "baseUrl": "https://test.app.rvd.ai",
+          "baseUrl": "https://dev.app.rvd.ai",
           "apis": {
             "ailearn": "https://ailearn-http-v2-j2rcifstnq-uc.a.run.app",
             "chat": "https://chat-api-http-v2-rb5caohzgq-uc.a.run.app"
