a6e866ab3cc837d8008f42ae2c75c7ed1bf294e1
Julio Rodriguez <jircdev@gmail.com>
2024-01-22 11:07:14 -0300

    fix: Solve problem with buttons

--- config.js
+++ config.js
@@ -1,40 +1,40 @@
 System.register([], function (_export, _context) {
-	'use strict';
+  "use strict";
 
-	return {
-		setters: [],
-		execute: function () {
-			_export('default', {
-				package: '@aimpact/ailearn-app',
-				version: '0.0.25-dev',
-				languages: {
-					default: 'en',
-					supported: ['en', 'es', 'pr'],
-				},
-				'global.css': true,
-				layout: 'ailearn-layout',
-				params: {
-					config: {
-						system: false,
-					},
-					container: 'file',
-					project: 'ailearn-estrada',
-					url: 'https://documents-rb5caohzgq-wl.a.run.app/upload',
-					DOCUMENTS_SERVER: 'https://documents-rb5caohzgq-wl.a.run.app',
-					CHAT_API_SERVER: 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload',
-					elevenlabs: {
-						key: 'ebe05ca0a4d9aafb4024d0666471a0c7',
-						id: 'pNInz6obpgDQGcFmaJgB',
-					},
-					baseUrl: 'https://dev.app.rvd.ai',
-					apis: {
-						ailearn: 'https://ailearn-http-v2-j2rcifstnq-uc.a.run.app',
-						chat: 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app',
-					},
-				},
-				ssr: {},
-				backend: {},
-			});
-		},
-	};
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", {
+        "package": "@aimpact/ailearn-app",
+        "version": "0.0.24",
+        "languages": {
+          "default": "en",
+          "supported": ["en", "es", "pr"]
+        },
+        "global.css": true,
+        "layout": "ailearn-layout",
+        "params": {
+          "config": {
+            "system": false
+          },
+          "container": "file",
+          "project": "ailearn-estrada",
+          "url": "https://documents-rb5caohzgq-wl.a.run.app/upload",
+          "DOCUMENTS_SERVER": "https://documents-rb5caohzgq-wl.a.run.app",
+          "CHAT_API_SERVER": "https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload",
+          "elevenlabs": {
+            "key": "ebe05ca0a4d9aafb4024d0666471a0c7",
+            "id": "pNInz6obpgDQGcFmaJgB"
+          },
+          "baseUrl": "https://dev.app.rvd.ai",
+          "apis": {
+            "ailearn": "https://ailearn-http-v2-j2rcifstnq-uc.a.run.app",
+            "chat": "https://chat-api-http-v2-rb5caohzgq-uc.a.run.app"
+          }
+        },
+        "ssr": {},
+        "backend": {}
+      });
+    }
+  };
 });
