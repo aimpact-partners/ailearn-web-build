2e8ad7c089ab758c9304eea1dd471048800ed1cd
Julio Rodriguez <jircdev@gmail.com>
2024-05-24 08:57:48 -0300

    feat: version 0.0.34

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/ailearn-app',
-				version: '0.0.33',
+				version: '0.0.34',
 				languages: {
 					default: 'en',
 					supported: ['en', 'es', 'pr'],
