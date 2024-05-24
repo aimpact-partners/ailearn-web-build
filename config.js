fd22846af4b5351e64f9c106bb088833c1b03f6c
Julio Rodriguez <jircdev@gmail.com>
2024-05-24 09:12:34 -0300

    feat: version 0.0.35

--- config.js
+++ config.js
@@ -1,42 +1,42 @@
 System.register([], function (_export, _context) {
-	'use strict';
+  "use strict";
 
-	return {
-		setters: [],
-		execute: function () {
-			_export('default', {
-				package: '@aimpact/ailearn-app',
-				version: '0.0.34',
-				languages: {
-					default: 'en',
-					supported: ['en', 'es', 'pr'],
-				},
-				environment: 'production',
-				'global.css': true,
-				params: {
-					config: {
-						system: false,
-					},
-					container: 'file',
-					project: 'ailearn-estrada',
-					APP_NAME: 'RVD.AI',
-					APP_LOGO: '/assets/rvd/rvd.png',
-					url: 'https://documents-rb5caohzgq-wl.a.run.app/upload',
-					DOCUMENTS_SERVER: 'https://documents-rb5caohzgq-wl.a.run.app',
-					CHAT_API_SERVER: 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload',
-					elevenlabs: {
-						key: 'ebe05ca0a4d9aafb4024d0666471a0c7',
-						id: 'pNInz6obpgDQGcFmaJgB',
-					},
-					baseUrl: 'https://app.rvd.ai',
-					apis: {
-						ailearn: 'https://ailearn-http-d7gw5k2ica-uc.a.run.app',
-						chat: 'https://chat-api-http-pjv2z6fura-uc.a.run.app',
-					},
-				},
-				ssr: {},
-				backend: {},
-			});
-		},
-	};
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", {
+        "package": "@aimpact/ailearn-app",
+        "version": "0.0.35.dev-01",
+        "languages": {
+          "default": "en",
+          "supported": ["en", "es", "pr"]
+        },
+        "environment": "production",
+        "global.css": true,
+        "params": {
+          "config": {
+            "system": false
+          },
+          "container": "file",
+          "project": "ailearn-estrada",
+          "APP_NAME": "RVD.AI",
+          "APP_LOGO": "/assets/rvd/rvd.png",
+          "url": "https://documents-rb5caohzgq-wl.a.run.app/upload",
+          "DOCUMENTS_SERVER": "https://documents-rb5caohzgq-wl.a.run.app",
+          "CHAT_API_SERVER": "https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload",
+          "elevenlabs": {
+            "key": "ebe05ca0a4d9aafb4024d0666471a0c7",
+            "id": "pNInz6obpgDQGcFmaJgB"
+          },
+          "baseUrl": "https://app.rvd.ai",
+          "apis": {
+            "ailearn": "https://ailearn-http-d7gw5k2ica-uc.a.run.app",
+            "chat": "https://chat-api-http-pjv2z6fura-uc.a.run.app"
+          }
+        },
+        "ssr": {},
+        "backend": {}
+      });
+    }
+  };
 });
