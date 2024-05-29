984fc08c1329be88f75efebd7d9558e809a0f914
Julio Rodriguez <jircdev@gmail.com>
2024-05-29 10:29:56 -0300

    feat: version 0.0.43.test-01

--- config.js
+++ config.js
@@ -1,1 +1,42 @@
-System.register([],function(a,t){"use strict";return{setters:[],execute:function(){a("default",{package:"@aimpact/ailearn-app",version:"0.0.42",languages:{default:"en",supported:["en","es","pr"]},environment:"testing","global.css":!0,params:{config:{system:!1},container:"file",project:"ailearn-estrada",APP_NAME:"RVD.AI",APP_LOGO:"/assets/rvd/rvd.png",url:"https://documents-rb5caohzgq-wl.a.run.app/upload",DOCUMENTS_SERVER:"https://documents-rb5caohzgq-wl.a.run.app",CHAT_API_SERVER:"https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload",elevenlabs:{key:"ebe05ca0a4d9aafb4024d0666471a0c7",id:"pNInz6obpgDQGcFmaJgB"},baseUrl:"https://test.app.rvd.ai",apis:{ailearn:"https://ailearn-http-v2-6dlchqr2za-uc.a.run.app",chat:"https://chat-api-http-skvkre4apq-uc.a.run.app"}},ssr:{},backend:{}})}}});
+System.register([], function (_export, _context) {
+	'use strict';
+
+	return {
+		setters: [],
+		execute: function () {
+			_export('default', {
+				package: '@aimpact/ailearn-app',
+				version: '0.0.43-test.01',
+				languages: {
+					default: 'en',
+					supported: ['en', 'es', 'pr'],
+				},
+				environment: 'testing',
+				'global.css': true,
+				params: {
+					config: {
+						system: false,
+					},
+					container: 'file',
+					project: 'ailearn-estrada',
+					APP_NAME: 'RVD.AI',
+					APP_LOGO: '/assets/rvd/rvd.png',
+					url: 'https://documents-rb5caohzgq-wl.a.run.app/upload',
+					DOCUMENTS_SERVER: 'https://documents-rb5caohzgq-wl.a.run.app',
+					CHAT_API_SERVER: 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload',
+					elevenlabs: {
+						key: 'ebe05ca0a4d9aafb4024d0666471a0c7',
+						id: 'pNInz6obpgDQGcFmaJgB',
+					},
+					baseUrl: 'https://test.app.rvd.ai',
+					apis: {
+						ailearn: 'https://ailearn-http-v2-6dlchqr2za-uc.a.run.app',
+						chat: 'https://chat-api-http-skvkre4apq-uc.a.run.app',
+					},
+				},
+				ssr: {},
+				backend: {},
+			});
+		},
+	};
+});
