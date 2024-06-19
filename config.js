336833e37975126cad7be80fae0036928ac1593e
Julio Rodriguez <jircdev@gmail.com>
2024-06-19 11:56:48 -0300

    version 0.0.46.test-24

--- config.js
+++ config.js
@@ -1,22 +1,16 @@
-System.register([], function (_export, _context) {
+System.register([], function (a, e) {
 	'use strict';
-
 	return {
 		setters: [],
 		execute: function () {
-			_export('default', {
+			a('default', {
 				package: '@aimpact/ailearn-app',
-				version: '0.0.46.test-22',
-				languages: {
-					default: 'en',
-					supported: ['en', 'es', 'pt']
-				},
+				version: '0.0.46.test-24',
+				languages: { default: 'en', supported: ['en', 'es', 'pt'] },
 				environment: 'testing',
-				'global.css': true,
+				'global.css': !0,
 				params: {
-					config: {
-						system: false
-					},
+					config: { system: !1 },
 					container: 'file',
 					project: 'ailearn-estrada',
 					APP_NAME: 'RVD.AI',
@@ -24,19 +18,14 @@ System.register([], function (_export, _context) {
 					url: 'https://documents-rb5caohzgq-wl.a.run.app/upload',
 					DOCUMENTS_SERVER: 'https://documents-rb5caohzgq-wl.a.run.app',
 					CHAT_API_SERVER: 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/upload',
-					beta: false,
-					elevenlabs: {
-						key: 'ebe05ca0a4d9aafb4024d0666471a0c7',
-						id: 'pNInz6obpgDQGcFmaJgB'
-					},
+					beta: !1,
+					elevenlabs: { key: 'ebe05ca0a4d9aafb4024d0666471a0c7', id: 'pNInz6obpgDQGcFmaJgB' },
 					baseUrl: 'https://test.app.rvd.ai',
-					apis: {
-						ailearn: 'https://test.ailearn.api.aimpact.partners'
-					}
+					apis: { ailearn: 'https://test.ailearn.api.aimpact.partners' },
 				},
 				ssr: {},
-				backend: {}
+				backend: {},
 			});
-		}
+		},
 	};
 });
---
+++ packages/@aimpact/chat-sdk@1.0.1/config.js
@@ -0,0 +1,32 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", {
+        "package": "@aimpact/chat-sdk",
+        "version": "1.0.1",
+        "languages": {
+          "default": "en",
+          "supported": ["en", "es", "pr"]
+        },
+        "environment": "testing",
+        "global.css": true,
+        "layout": "main-layout",
+        "params": {
+          "elevenlabs": {
+            "key": "ebe05ca0a4d9aafb4024d0666471a0c7",
+            "id": "pNInz6obpgDQGcFmaJgB"
+          },
+          "baseUrl": "https://test.app.rvd.ai",
+          "apis": {
+            "chat": "https://chat-api-http-skvkre4apq-uc.a.run.app"
+          }
+        },
+        "ssr": {},
+        "backend": {}
+      });
+    }
+  };
+});
