900b32030b565f70d952ba955c1c974ebb84f623
Julio Rodríguez <jircdev@gmail.com>
2024-07-08 10:57:02 -0300

    feat: config.js

---
+++ packages/@aimpact/auth@0.0.1/config.js
@@ -0,0 +1,24 @@
+System.register([], function (_export, _context) {
+	'use strict';
+
+	return {
+		setters: [],
+		execute: function () {
+			_export('default', {
+				package: '@aimpact/ailearn-sdk',
+				version: '1.0.0',
+				languages: {
+					default: 'en',
+					supported: ['en', 'es'],
+				},
+				environment: 'production',
+				'global.css': true,
+				params: {
+					baseUrl: 'https://app.rvd.ai',
+				},
+				ssr: {},
+				backend: {},
+			});
+		},
+	};
+});
