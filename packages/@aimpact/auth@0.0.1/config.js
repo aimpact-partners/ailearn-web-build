bb8b7dbf9d8ea748f5cc6cbff607f2dccb5db2bc
Julio Rodriguez <jircdev@gmail.com>
2024-08-05 13:32:49 -0300

    feat: version 0.1.1-dev.1

--- packages/@aimpact/auth@0.0.1/config.js
+++ packages/@aimpact/auth@0.0.1/config.js
@@ -1,19 +1,23 @@
 System.register([], function (_export, _context) {
-	'use strict';
+  "use strict";
 
-	return {
-		setters: [],
-		execute: function () {
-			_export('default', {
-				package: '@aimpact/auth',
-				version: '1.0.0',
-				params: {},
-				languages: {
-					default: 'en',
-					supported: ['en', 'es']
-				},
-				environment: 'development'
-			});
-		}
-	};
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", {
+        "package": "@aimpact/auth",
+        "version": "0.0.1",
+        "languages": {
+          "default": "en",
+          "supported": ["en", "es"]
+        },
+        "environment": "development",
+        "global.css": true,
+        "layout": "main-layout",
+        "params": {},
+        "ssr": {},
+        "backend": {}
+      });
+    }
+  };
 });
