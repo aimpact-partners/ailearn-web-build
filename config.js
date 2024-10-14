c9c1d01800d051dd0ef43211d450d7ff9769f884
Julio Rodriguez <jircdev@gmail.com>
2024-10-14 12:17:52 -0300

    feat: verison 0.1.13-dev.25

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/ailearn-app',
-				version: '0.1.13-dev.24',
+				version: '0.1.13-dev.25',
 				languages: {
 					default: 'en',
 					supported: ['en', 'es', 'pt']
