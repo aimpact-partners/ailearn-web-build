01d49dac77b1b20b37ab6584a1d460004e8d6ff7
Julio Rodriguez <jircdev@gmail.com>
2024-11-26 21:42:36 -0300

    feat: version 0.2.10-beta.09

--- packages/@beyond-js/reactive@1.1.12/config.js
+++ packages/@beyond-js/reactive@1.1.12/config.js
@@ -20,26 +20,4 @@ System.register([], function (_export, _context) {
       });
     }
   };
-});System.register([], function (_export, _context) {
-  "use strict";
-
-  return {
-    setters: [],
-    execute: function () {
-      _export("default", {
-        "package": "@beyond-js/reactive",
-        "version": "1.1.12",
-        "languages": {
-          "default": "en",
-          "supported": ["en", "es"]
-        },
-        "environment": "development",
-        "global.css": true,
-        "layout": "reactive-layout",
-        "params": {},
-        "ssr": {},
-        "backend": {}
-      });
-    }
-  };
 });
