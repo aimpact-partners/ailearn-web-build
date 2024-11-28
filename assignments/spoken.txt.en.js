8c6d0e51fbe2d8178a8605e55e3ebe5f70bd9e90
Julio Rodriguez <jircdev@gmail.com>
2024-11-28 18:46:11 -0300

    feat: version 0.2.10-beta.18

--- assignments/spoken.txt.en.js
+++ assignments/spoken.txt.en.js
@@ -29,7 +29,7 @@ System.register(["@beyond-js/kernel@0.1.9/bundle"], function (_export, _context)
       INTERNAL MODULE: ./txt
       *********************/
       ims.set('./txt', {
-        hash: 3559702411,
+        hash: 3740695603,
         creator: function (require, exports) {
           exports.txt = {
             "breadcrumb": {
@@ -54,7 +54,7 @@ System.register(["@beyond-js/kernel@0.1.9/bundle"], function (_export, _context)
             },
             "tabs": {
               "analysis": "Activity",
-              "activity": "Information"
+              "activity": "TASK"
             },
             "assessments": {
               "quiz": "Assessment",
--- assignments/spoken.txt.es.js
+++ assignments/spoken.txt.es.js
@@ -29,7 +29,7 @@ System.register(["@beyond-js/kernel@0.1.9/bundle"], function (_export, _context)
       INTERNAL MODULE: ./txt
       *********************/
       ims.set('./txt', {
-        hash: 1533886254,
+        hash: 1266530394,
         creator: function (require, exports) {
           exports.txt = {
             "breadcrumb": {
@@ -54,7 +54,7 @@ System.register(["@beyond-js/kernel@0.1.9/bundle"], function (_export, _context)
             },
             "tabs": {
               "analysis": "Actividad",
-              "activity": "Información"
+              "activity": "Tarea"
             },
             "assessments": {
               "quiz": "Evaluación",
