80b847b040b0681b06aa4d4c043a036a03033185
Julio Rodriguez <jircdev@gmail.com>
2025-03-07 13:27:20 -0300

    feat: version 0.3.16-dev.18

--- classrooms/join.txt.es.js
+++ classrooms/join.txt.es.js
@@ -29,7 +29,7 @@ System.register(["@beyond-js/kernel@0.1.9/bundle"], function (_export, _context)
       INTERNAL MODULE: ./txt
       *********************/
       ims.set('./txt', {
-        hash: 874807691,
+        hash: 1753067128,
         creator: function (require, exports) {
           exports.txt = {
             "breadcrumb": {
@@ -63,7 +63,8 @@ System.register(["@beyond-js/kernel@0.1.9/bundle"], function (_export, _context)
             },
             "messages": {
               "authorized": "Ya tienes acceso al aula",
-              "pending,": "Tu solicitud de acceso está pendiente de aprobación por el profesor"
+              "pending,": "Tu solicitud de acceso está pendiente de aprobación por el profesor",
+              "pending": "Tu solicitud de acceso está pendiente de aprobación por el profesor"
             },
             "ready": {
               "title": "Solicitud enviada",
--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/ailearn-app',
-				version: '0.3.16-dev.17',
+				version: '0.3.16-dev.18',
 				languages: {
 					default: 'en',
 					supported: ['en', 'es', 'pt']
