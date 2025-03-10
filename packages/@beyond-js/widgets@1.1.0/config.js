23fa5a265b7f2d7c97e232cce74b59cf4f3bdcd3
Julio Rodríguez <jircdev@gmail.com>
2025-03-10 18:25:01 -0300

    feat: version 0.6.23'

---
+++ packages/@beyond-js/widgets@1.1.0/config.js
@@ -0,0 +1,22 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", {
+        "package": "@beyond-js/widgets",
+        "version": "1.1.0",
+        "languages": {
+          "supported": [],
+          "default": "en"
+        },
+        "environment": "development",
+        "global.css": true,
+        "params": {},
+        "ssr": {},
+        "backend": {}
+      });
+    }
+  };
+});
