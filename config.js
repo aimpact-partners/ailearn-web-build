f881338d8aebc4eb338c4198bad1a2ae2de9f668
Julio Rodriguez <jircdev@gmail.com>
2025-03-24 19:42:04 -0300

    feat: version 0.3.29-beta-04

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ System.register([], function (a, e) {
 		execute: function () {
 			a('default', {
 				package: '@aimpact/ailearn-app',
-				version: '0.3.29-beta.03',
+				version: '0.3.29-beta.04',
 				languages: { default: 'en', supported: ['en', 'es'] },
 				environment: 'quality',
 				'global.css': !0,
