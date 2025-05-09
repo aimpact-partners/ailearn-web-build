a5aeca73afd06de4954739bca92dbcd64b7b19c7
Julio Rodriguez <jircdev@gmail.com>
2025-05-09 18:07:03 -0300

    feat: version 0.4.1-dev.13

--- config.js
+++ config.js
@@ -1,37 +1,28 @@
-System.register([], function (a, e) {
-	return {
-		setters: [],
-		execute: function () {
-			a('default', {
-				package: '@aimpact/rvd',
-				version: '0.4.1-beta.12',
-				languages: { default: 'en', supported: ['en', 'es', 'de'] },
-				environment: 'quality',
-				'global.css': !0,
-				params: {
-					app: 'rvd',
-					APP_LOGO: '/assets/images/branding/logo.png',
-					APP_NAME: 'AILearn',
-					APP_SUPPORT_EMAIL: 'soporte@rvd.ai',
-					tutorials: !0,
-					baseUrl: 'https://beta.app.rvd.ai',
-					apis: {
-						ailearn: 'https://beta.ailearn.api.aimpact.partners',
-						agents: 'https://beta.agents.api.aimpact.partners'
-					},
-					firebase: {
-						baseUrl: 'https://beta.app.rvd.ai',
-						apiKey: 'AIzaSyAEEozzwR0cguqynxvL1qB38i8liOCgtJc',
-						authDomain: 'aimpact-partners-prod.firebaseapp.com',
-						projectId: 'aimpact-partners-prod',
-						storageBucket: 'aimpact-partners-prod.appspot.com',
-						messagingSenderId: '741854278426',
-						appId: '1:741854278426:web:9ff2a50e705edcc501f2a3'
-					}
-				},
-				ssr: {},
-				backend: {}
-			});
-		}
-	};
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", {
+        "package": "@aimpact/ailearn-app",
+        "version": "0.4.1",
+        "languages": {
+          "default": "en",
+          "supported": ["en", "es", "de"]
+        },
+        "environment": "quality",
+        "global.css": true,
+        "params": {
+          "container": "file",
+          "project": "rvd",
+          "APP_NAME": "Ailearn",
+          "APP_LOGO": "branding/logo.png",
+          "url": "https://documents-rb5caohzgq-wl.a.run.app/upload"
+        },
+        "ssr": {},
+        "backend": {}
+      });
+    }
+  };
 });
