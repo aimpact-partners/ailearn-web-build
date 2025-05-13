1a0230dc568a9dea21856c7c3045657adc4fc637
Julio Rodriguez <jircdev@gmail.com>
2025-05-13 00:20:58 -0300

    feat: version 0.4.4

--- config.js
+++ config.js
@@ -1,24 +1,19 @@
-System.register([], function (_export, _context) {
-	'use strict';
-
+System.register([], function (a, e) {
 	return {
 		setters: [],
 		execute: function () {
-			_export('default', {
+			a('default', {
 				package: '@aimpact/rvd',
-				version: '0.4.3',
-				languages: {
-					default: 'en',
-					supported: ['en', 'es', 'de']
-				},
+				version: '0.4.4',
+				languages: { default: 'en', supported: ['en', 'es', 'de'] },
 				environment: 'production',
-				'global.css': true,
+				'global.css': !0,
 				params: {
 					app: 'rvd',
 					APP_LOGO: '/assets/images/branding/logo.png',
 					APP_NAME: 'AILearn',
 					APP_SUPPORT_EMAIL: 'soporte@rvd.ai',
-					tutorials: true,
+					tutorials: !0,
 					baseUrl: 'https://app.rvd.ai',
 					apis: {
 						ailearn: 'https://ailearn.api.aimpact.partners',
