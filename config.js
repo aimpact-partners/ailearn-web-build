89545d9bd8655929f0474c0be42d317b3d9ff2fc
Julio Rodriguez <jircdev@gmail.com>
2025-05-23 16:56:47 -0300

    feat: version 0.4.12-test.03

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/rvd',
-				version: '0.4.5',
+				version: '0.4.12-test.03',
 				languages: {
 					default: 'en',
 					supported: ['en', 'es', 'de']
--- packages/@aimpact/media-manager@1.0.0/uploader.js
+++ packages/@aimpact/media-manager@1.0.0/uploader.js
@@ -18,7 +18,7 @@ System.register(["@beyond-js/kernel@0.1.12/bundle", "@beyond-js/reactive@2.0.5/m
     }],
     execute: function () {
       bimport = specifier => {
-        const dependencies = new Map([["pragmate-ui", "1.0.0-beta.7"], ["@beyond-js/backend", "0.1.9"], ["@beyond-js/events", "0.0.7"], ["@beyond-js/kernel", "0.1.9"], ["@beyond-js/local", "0.1.3"], ["@beyond-js/react-18-widgets", "1.1.2"], ["@beyond-js/reactive", "1.1.13"], ["socket.io-client", "4.8.1"], ["@aimpact/media-manager", "1.0.0"], ["@aimpact/rvd", "0.4.1"]]);
+        const dependencies = new Map([["pragmate-ui", "1.0.0-beta.7"], ["@beyond-js/backend", "0.1.9"], ["@beyond-js/events", "0.0.7"], ["@beyond-js/kernel", "0.1.9"], ["@beyond-js/local", "0.1.3"], ["@beyond-js/react-18-widgets", "1.1.2"], ["@beyond-js/reactive", "1.1.13"], ["socket.io-client", "4.8.1"], ["@aimpact/media-manager", "1.0.0"], ["@aimpact/rvd", "0.4.12"]]);
         return globalThis.bimport(globalThis.bimport.resolve(specifier, dependencies));
       };
       ({
