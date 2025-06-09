99f0f32e66d7f44f907922d30749576025d99482
Julio Rodriguez <jircdev@gmail.com>
2025-06-09 12:21:09 -0300

    feat: version 0.4.14-beta,03

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
 System.register([], function (_export, _context) {
-  "use strict";
+	'use strict';
 
-  return {
-    setters: [],
-    execute: function () {
-      _export("default", {
-        "package": "@aimpact/rvd",
-        "version": "0.4.14-beta.02",
-        "languages": {
-          "default": "en",
-          "supported": ["en", "es", "de"]
-        },
-        "environment": "quality",
-        "global.css": true,
-        "params": {
-          "app": "rvd",
-          "APP_LOGO": "/assets/images/branding/logo.png",
-          "APP_NAME": "AILearn",
-          "APP_SUPPORT_EMAIL": "soporte@rvd.ai",
-          "tutorials": true,
-          "baseUrl": "https://beta.app.rvd.ai",
-          "apis": {
-            "ailearn": "https://beta.ailearn.api.aimpact.partners",
-            "agents": "https://beta.agents.api.aimpact.partners",
-            "realtime": "wss://beta.agents.api.aimpact.partners"
-          },
-          "firebase": {
-            "baseUrl": "https://beta.app.rvd.ai",
-            "apiKey": "AIzaSyAEEozzwR0cguqynxvL1qB38i8liOCgtJc",
-            "authDomain": "aimpact-partners-prod.firebaseapp.com",
-            "projectId": "aimpact-partners-prod",
-            "storageBucket": "aimpact-partners-prod.appspot.com",
-            "messagingSenderId": "741854278426",
-            "appId": "1:741854278426:web:9ff2a50e705edcc501f2a3"
-          }
-        },
-        "ssr": {},
-        "backend": {}
-      });
-    }
-  };
+	return {
+		setters: [],
+		execute: function () {
+			_export('default', {
+				package: '@aimpact/rvd',
+				version: '0.4.14-beta.03',
+				languages: {
+					default: 'en',
+					supported: ['en', 'es', 'de']
+				},
+				environment: 'quality',
+				'global.css': true,
+				params: {
+					app: 'rvd',
+					APP_LOGO: '/assets/images/branding/logo.png',
+					APP_NAME: 'AILearn',
+					APP_SUPPORT_EMAIL: 'soporte@rvd.ai',
+					tutorials: true,
+					baseUrl: 'https://beta.app.rvd.ai',
+					apis: {
+						ailearn: 'https://beta.ailearn.api.aimpact.partners',
+						agents: 'https://beta.agents.api.aimpact.partners',
+						realtime: 'wss://beta.agents.api.aimpact.partners'
+					},
+					firebase: {
+						baseUrl: 'https://beta.app.rvd.ai',
+						apiKey: 'AIzaSyAEEozzwR0cguqynxvL1qB38i8liOCgtJc',
+						authDomain: 'aimpact-partners-prod.firebaseapp.com',
+						projectId: 'aimpact-partners-prod',
+						storageBucket: 'aimpact-partners-prod.appspot.com',
+						messagingSenderId: '741854278426',
+						appId: '1:741854278426:web:9ff2a50e705edcc501f2a3'
+					}
+				},
+				ssr: {},
+				backend: {}
+			});
+		}
+	};
 });
