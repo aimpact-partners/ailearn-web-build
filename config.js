94e5e1f63ad63173160fe9f228352cae40e364b9
Julio Rodriguez <jircdev@gmail.com>
2025-10-07 15:52:51 -0300

    feat: version 0.6.6-beta.01

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ System.register([], function (_export, _context) {
 		execute: function () {
 			_export('default', {
 				package: '@aimpact/rvd',
-				version: '0.6.6',
+				version: '0.6.6-beta.01',
 				languages: { default: 'en', supported: ['en', 'es', 'de', 'pt', 'fr'] },
 				environment: 'quality',
 				'global.css': !0,
