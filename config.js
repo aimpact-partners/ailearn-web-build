105fe80fab419db8aa4f22764db53f6f6626dcd7
Julio Rodriguez <jircdev@gmail.com>
2025-11-03 00:30:54 -0300

    feat: version 0.7.3

--- config.js
+++ config.js
@@ -1,47 +1,47 @@
 System.register([], function (_export, _context) {
-  "use strict";
+	'use strict';
 
-  return {
-    setters: [],
-    execute: function () {
-      _export("default", {
-        "package": "@aimpact/rvd",
-        "version": "0.7.2",
-        "languages": {
-          "default": "en",
-          "supported": ["en", "es", "de", "pt", "fr"]
-        },
-        "environment": "production",
-        "global.css": true,
-        "params": {
-          "app": "rvd",
-          "APP_LOGO": "/assets/images/branding/logo.png",
-          "APP_NAME": "RVD AI",
-          "APP_SUPPORT_EMAIL": "soporte@rvd.ai",
-          "tutorials": true,
-          "baseUrl": "https://app.rvd.ai",
-          "apis": {
-            "ailearn": "https://ailearn.api.aimpact.partners",
-            "agents": "https://agents.api.aimpact.partners",
-            "realtime": "wss://agents.api.aimpact.partners"
-          },
-          "ailearn": "https://dev.ailearn.api.aimpact.partners",
-          "agents": {
-            "projectId": "a1l34rn1-453a-4612-b6fd-59cb742111d0"
-          },
-          "firebase": {
-            "baseUrl": "https://app.rvd.ai",
-            "apiKey": "AIzaSyAx9lNe3j-LBqdqhAVl3ytmZxUI3xcAMmo",
-            "authDomain": "ailearn-prod-404415.firebaseapp.com",
-            "projectId": "ailearn-prod-404415",
-            "storageBucket": "ailearn-prod-404415.firebasestorage.app",
-            "messagingSenderId": "755928984767",
-            "appId": "1:755928984767:web:d9fc0405944137fdf0ea0a"
-          }
-        },
-        "ssr": {},
-        "backend": {}
-      });
-    }
-  };
+	return {
+		setters: [],
+		execute: function () {
+			_export('default', {
+				package: '@aimpact/rvd',
+				version: '0.7.3',
+				languages: {
+					default: 'en',
+					supported: ['en', 'es', 'de', 'pt', 'fr']
+				},
+				environment: 'production',
+				'global.css': true,
+				params: {
+					app: 'rvd',
+					APP_LOGO: '/assets/images/branding/logo.png',
+					APP_NAME: 'RVD AI',
+					APP_SUPPORT_EMAIL: 'soporte@rvd.ai',
+					tutorials: true,
+					baseUrl: 'https://app.rvd.ai',
+					apis: {
+						ailearn: 'https://ailearn.api.aimpact.partners',
+						agents: 'https://agents.api.aimpact.partners',
+						realtime: 'wss://agents.api.aimpact.partners'
+					},
+					ailearn: 'https://dev.ailearn.api.aimpact.partners',
+					agents: {
+						projectId: 'a1l34rn1-453a-4612-b6fd-59cb742111d0'
+					},
+					firebase: {
+						baseUrl: 'https://app.rvd.ai',
+						apiKey: 'AIzaSyAx9lNe3j-LBqdqhAVl3ytmZxUI3xcAMmo',
+						authDomain: 'ailearn-prod-404415.firebaseapp.com',
+						projectId: 'ailearn-prod-404415',
+						storageBucket: 'ailearn-prod-404415.firebasestorage.app',
+						messagingSenderId: '755928984767',
+						appId: '1:755928984767:web:d9fc0405944137fdf0ea0a'
+					}
+				},
+				ssr: {},
+				backend: {}
+			});
+		}
+	};
 });
