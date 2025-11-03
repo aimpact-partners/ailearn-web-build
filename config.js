c63903ae91a0a1b187a6fbb22da4c9576318699d
Julio Rodriguez <jircdev@gmail.com>
2025-11-03 12:25:40 -0300

    chore: version 0.7.4-beta.02

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ System.register([], function (a, e) {
 		execute: function () {
 			a('default', {
 				package: '@aimpact/rvd',
-				version: '0.7.4-beta.01',
+				version: '0.7.4-beta.02',
 				languages: { default: 'en', supported: ['en', 'es', 'de', 'pt', 'fr'] },
 				environment: 'quality',
 				'global.css': !0,
